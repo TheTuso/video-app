66cf3cbae82ef63202081b900a73d85acce77a44
TheTuso <piotrekpasztor@gmail.com>
2025-11-08 17:41:51 +0100

    feat: implement font loading and splash screen handling for improved user experience

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import { useFonts } from '@expo-google-fonts/poppins';
 import { Stack } from 'expo-router';
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { FONTS } from '@/utils/fonts';
+
+// Prevent the splash screen from auto-hiding before assets have loaded.
+preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [loaded, error] = useFonts(FONTS);
+
+	useEffect(() => {
+		if (loaded || error) {
+			// Hide the splash screen once the fonts have loaded.
+			hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return <Stack screenOptions={{ headerShown: false }} />;
 }
