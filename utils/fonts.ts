d78d35584db9aa290217b0fc678e8d3aa15375c1
TheTuso <piotrekpasztor@gmail.com>
2025-11-09 20:38:59 +0100

    feat: add Poppins_700Bold font to FONTS export for enhanced typography options

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -2,10 +2,12 @@ import {
 	Poppins_400Regular,
 	Poppins_500Medium,
 	Poppins_600SemiBold,
+	Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
 export const FONTS = {
 	Poppins_400Regular,
 	Poppins_600SemiBold,
 	Poppins_500Medium,
+	Poppins_700Bold,
 };
