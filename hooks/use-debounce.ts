574418b784ec83227ac5125c3bb23a7dde61d0fe
TheTuso <piotrekpasztor@gmail.com>
2025-11-10 13:35:47 +0100

    feat: add useDebounce hook for improved input handling and performance

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,18 @@
+// Hook from https://github.com/uidotdev/usehooks
+import { useEffect, useState } from 'react';
+
+export function useDebounce(value: string, delay: number) {
+	const [debouncedValue, setDebouncedValue] = useState(value);
+
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebouncedValue(value);
+		}, delay);
+
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [value, delay]);
+
+	return debouncedValue;
+}
